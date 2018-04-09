a100d86a926137e5eb1ce26a670b1a2a45c9f153
Micheal Arsenault <mike2ykme@gmail.com>
2018-04-09 10:24:30 -0500

    added logic and ability to export to single jar

---
+++ src/main/java/com/icrn/substitutes/Substitutes.java
@@ -0,0 +1,17 @@
+package com.icrn.substitutes;
+
+import com.icrn.substitutes.dao.RequestRepositoryInMemory;
+import com.icrn.substitutes.dao.SubstituteRepositoryInMemory;
+import com.icrn.substitutes.dao.UserRepositoryInMemory;
+
+public class Substitutes {
+    public static void main(String ...args){
+//
+//        Controller controller = new Controller(
+//                new RequestRepositoryInMemory(),
+//                new SubstituteRepositoryInMemory(),
+//                new UserRepositoryInMemory());
+//
+//        System.out.println(controller.getAllSubstitutes());
+    }
+}
